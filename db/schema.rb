1f5b509105bef258568383973d34c61d9f62ce90
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-03-06 18:02:36 +0300

    Display pizza description in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_111437) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_125949) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
