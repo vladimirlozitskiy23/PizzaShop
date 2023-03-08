8f76fcd812156ecc266b3079898dbdc90db8e5be
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-03-08 14:46:00 +0300

    Add localStorage

--- public/app.js
+++ public/app.js
@@ -2,3 +2,10 @@ function something()
 {
   alert("Hello!!!");
 }
+
+function addToCart(id) 
+{
+  var x  = window.localStorage.getItem('product_' + id);
+  x = x * 1 + 1;
+  window.localStorage.setItem('product_' + id, x); 
+}
