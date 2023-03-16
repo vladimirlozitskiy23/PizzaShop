4941f6071629e9ef8f22daf5f640c6bf19ca14b9
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-03-16 17:40:33 +0300

    Add cartNumberGetItem function

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
+function test()
+{
+  alert("test");
+}
+
 function something()
 {
   alert("Hello!!!");
@@ -8,4 +13,20 @@ function addToCart(id)
   var x  = window.localStorage.getItem('product_' + id);
   x = x * 1 + 1;
   window.localStorage.setItem('product_' + id, x); 
+  alert("Items in your cart" + cartNumberGetItem());
+}
+
+function cartNumberGetItem()
+{
+  cnt = 0;
+  for(i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+    if (key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1 ;
+    }
+  }
+  return cnt;
 }
